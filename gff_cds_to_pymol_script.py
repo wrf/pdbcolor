309512ffd841d909bbd68eeddc2220c54cd18374
wrf <wrfbiolum@protonmail.com>
2022-02-03 12:49:31 +0100

    change color order for better contrast

--- gff_cds_to_pymol_script.py
+++ gff_cds_to_pymol_script.py
@@ -2,7 +2,7 @@
 #
 # gff_cds_to_pymol_script.py v1 2018-06-04
 
-'''gff_cds_to_pymol_script.py  last modified 2022-02-01
+'''gff_cds_to_pymol_script.py  last modified 2022-02-03
     script to generate a PyMOL script to color residues corresponding to exons
     20 colors are currently available, and repeat after 20 exons
 
@@ -33,23 +33,19 @@ import argparse
 def make_output_script(cds_to_residues, out_of_phase_list, chainset, wayout):
 	'''from the GFF information, print a script for PyMOL'''
 
-	# colors in order of:
-	# blue, green, red, orange, purple
-	# brown, teal, lilac, seagreen, yellow
-	exon_colors = { 1 :[0.65,0.81,0.89], 2 :[0.12,0.47,0.71], 
-                    3 :[0.70,0.87,0.54], 4 :[0.20,0.63,0.17], 
-                    5 :[0.98,0.60,0.60], 6 :[0.89,0.10,0.11], 
-                    7 :[0.99,0.75,0.44], 8 :[1.00,0.50,0.00], 
-                    9 :[0.79,0.70,0.84], 10 :[0.42,0.24,0.60], 
-                    11 :[0.90,0.65,0.50], 12 :[0.67,0.35,0.14], 
-                    13 :[0.19,0.91,0.94], 14 :[0.00,0.72,0.75], 
-                    15 :[0.89,0.65,0.82], 16 :[0.71,0.12,0.48], 
-                    17 :[0.65,0.89,0.76], 18 :[0.12,0.71,0.45], 
-                    19 :[0.89,0.88,0.65], 20 :[0.71,0.67,0.12] }
-	# { 1:"#a6cee3", 2:"#1f78b4", 3:"#b2df8a", 4:"#33a02c", 5:"#fb9a99", 
-	# 6:"#e31a1c", 7:"#fdbf6f", 8:"#ff7f00", 9:"#cab2d6", 10:"#6a3d9a", 
-	# 11:"#e5a580", 12:"#aa5823", 13:"#30e7ef", 14:"#00b7c0", 15:"#e3a6d1", 
-	# 16:"#b41f7a", 17:"#a6e3c1", 18:"#1fb472", 19:"#e3e0a6", 20:"#b4ac1f" }
+	# colors alternating light and dark, in order of:
+	default_colors = [ [0.65,0.81,0.89], [0.12,0.47,0.71], # blue "#a6cee3", "#1f78b4",
+		          [0.99,0.75,0.44], [1.00,0.50,0.00], # orange "#fdbf6f","#ff7f00",
+		          [0.70,0.87,0.54], [0.20,0.63,0.17], # green "#b2df8a", "#33a02c",
+		          [0.79,0.70,0.84], [0.42,0.24,0.60], # purple "#cab2d6", "#6a3d9a",
+		          [0.98,0.60,0.60], [0.89,0.10,0.11], # red "#fb9a99", "#e31a1c",
+		          [0.19,0.91,0.94], [0.00,0.72,0.75], # teal "#30e7ef", "#00b7c0",
+		          [0.90,0.65,0.50], [0.67,0.35,0.14], # brown "#e5a580", "#aa5823",
+		          [0.89,0.65,0.82], [0.71,0.12,0.48], # lilac "#e3a6d1", "#b41f7a",
+		          [0.65,0.89,0.76], [0.12,0.71,0.45], # seagreen "#a6e3c1", "#1fb472"
+		          [0.89,0.88,0.65], [0.71,0.67,0.12] ] # yellow "#e3e0a6", "#b4ac1f"
+	exon_numbers = list( range(1, len(default_colors)+1 ) )
+	exon_colors = dict( zip( exon_numbers, default_colors ) )
 
 	outofphase_color = [0.3,0.3,0.3] # dark grey
 	numcolors = len(exon_colors)
@@ -60,6 +56,7 @@ def make_output_script(cds_to_residues, out_of_phase_list, chainset, wayout):
 	wayout.write("bg white\n")
 	wayout.write("show cartoon, ({})\n".format(chainstring) )
 	for exonnum, residues in enumerate(cds_to_residues):
+		# mod function of number of colors, meaning colors recycle after numcolors, so after 20 exons
 		colorlist = map(str, exon_colors[(exonnum % numcolors)+1])
 		wayout.write("set_color excolor_{}, [{}]\n".format( exonnum+1, ",".join(colorlist) ) )
 		wayout.write("select exon_{}, (resi {}) & ({})\n".format( exonnum+1, "-".join(residues), chainstring ) )
@@ -78,14 +75,15 @@ def attributes_to_dict(attributes):
 	attrd = dict([(field.strip().split("=")) for field in attributes.split(";") if field.count("=")])
 	return attrd
 
-def get_gff_exons(gfffile, gffid, residue_offset=0):
+def get_gff_exons(gfffile, gffid, residue_offset):
 	'''read GFF file, extract exon/CDS information for a specified gene, return a list of lists of residues as strings'''
 	#TODO child_to_parent = {} # 
 
 	# read through GFF, pull out all CDS features matching the ID, store length in bases
 	linecounter = 0
-	cdscounter = 0
-	exon_lengths = {}
+	exon_counter = 0 # count exons, to report in case there are entire UTR exons
+	cdscounter = 0 # count CDS features in the GFF
+	cds_feat_lengths = {}
 	gff_ids_from_file = {} # list of the IDs found in the file, for error checking, key is ID, value is True
 	sys.stderr.write("# Parsing GFF file {} for {}  {}\n".format( gfffile, gffid , time.asctime() ) )
 	for line in open(gfffile,'r'):
@@ -96,35 +94,43 @@ def get_gff_exons(gfffile, gffid, residue_offset=0):
 			attrstring = lsplits[8]
 			attr_dict = attributes_to_dict(attrstring)
 			cds_id = attr_dict.get("ID",False)
+			parent_id = attr_dict.get("Parent",False)
+			gene_id = attr_dict.get("gene",False)
 			if cds_id:
 				gff_ids_from_file[cds_id] = True
-			if cds_id==gffid or attr_dict.get("Parent",False)==gffid:
+			if cds_id==gffid or parent_id==gffid or gene_id==gffid:
 				feature = lsplits[2]
 				strand = lsplits[6]
 
 				if feature == "CDS":
-					cdscounter += 1
+					cdscounter += 1 # first CDS feature will be 1, not 0
 					exonlen = int(lsplits[4]) - int(lsplits[3]) + 1
 					if strand=="+":
-						exon_lengths[cdscounter] = exonlen
+						cds_feat_lengths[cdscounter] = exonlen
 					elif strand=="-": # make numbers negative, so will be sorted backwards
-						exon_lengths[-1*cdscounter] = exonlen
+						cds_feat_lengths[-1*cdscounter] = exonlen
 					else: # for anything else, as everything should have a strand
 						sys.stderr.write("# WARNING UNKNOWN STRAND: {} FOR\n{}\n".format(strand, line) )
+				elif feature=="exon":
+					exon_counter += 1
 	sys.stderr.write("# Finished parsing {}, read {} lines  {}\n".format(gfffile, linecounter, time.asctime() ) )
 	if cdscounter==0: # could not find any CDS features, or ID is wrong
 		sys.stderr.write("# WARNING COULD NOT FIND ANY CDS FEATURES FOR {}\n".format(gffid) )
 		if gff_ids_from_file: # list IDs that were found
-			sys.stderr.write("# FILE CONTAINS ID={}\n".format( " ".join(gff_ids_from_file.keys()) ) )
+			sys.stderr.write("# FILE CONTAINS FOLLOWING IDs: {}\n".format( " ".join(gff_ids_from_file.keys()) ) )
 
 	# sort CDS features in order, depending on strand, and count amino acids per exon
-	basecounter = 0
-	residuecounter = residue_offset
+	basecounter = residue_offset*3 # should be 0 by default
+	residuecounter = residue_offset # also 0 by default
+
 	cds_list = [] # list, of lists of residue numbers, as strings
 	out_of_phase = [] # all residues that are out of phase, colored grey
+	## NOTE: this does NOT depend on the phase column lsplits[7] in the GFF,
+	## the phase is calculated from the bases added by each exon
+
 	# append a residue start-end pair to cds_list
-	for exonnumber in sorted(exon_lengths.keys()):
-		cdslength = exon_lengths[exonnumber]
+	for exonnumber in sorted(cds_feat_lengths.keys()):
+		cdslength = cds_feat_lengths[exonnumber]
 		basecounter += cdslength
 		firstresidue = residuecounter + 1
 		residuecounter = (basecounter // 3) # add all integer residues
@@ -138,8 +144,10 @@ def get_gff_exons(gfffile, gffid, residue_offset=0):
 			residuecounter += 1 # add 1 for the out of phase
 			out_of_phase.append( str(residuecounter) )
 
+	if exon_counter:
+		sys.stderr.write("# Counted {} exons for {}\n".format( exon_counter, gffid ) )
 	if cdscounter:
-		sys.stderr.write("# Found {} CDS exons, {} bases for {} residues\n".format( cdscounter, basecounter, residuecounter ) )
+		sys.stderr.write("# Found {} CDS features, {} bases for {} residues\n".format( cdscounter, basecounter, residuecounter ) )
 		sys.stderr.write("# {} codons are out of phase\n".format( len(out_of_phase) ) )
 	return cds_list, out_of_phase
 
@@ -149,10 +157,11 @@ def main(argv, wayout):
 	parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description=__doc__)
 	parser.add_argument("-c","--chain", default="A", help="chain used in PDB format file [A]")
 	parser.add_argument("-g","--gff", help="GFF file that contains genes and CDS entries for -s", required=True)
-	parser.add_argument("-i","--id", help="ID of sequence in the GFF file, if ID or Parent field does not match -s")
+	parser.add_argument("-i","--id", help="ID of sequence in the GFF file, could match either ID or Parent tag")
+	parser.add_argument("-r","--residue-offset", type=int, default=0, help="number of residues to offset for the start of the structure, for example, if using a structure with 6H tag, etc [default is 0]")
 	args = parser.parse_args(argv)
 
-	residue_list, outofphase_list = get_gff_exons(args.gff, args.id)
+	residue_list, outofphase_list = get_gff_exons(args.gff, args.id, args.residue_offset)
 	make_output_script(residue_list, outofphase_list, args.chain, wayout)
 
 if __name__ == "__main__":
